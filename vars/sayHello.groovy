55f25c09ba0dbf29204236a6ca679ea7c3bc8efe
yossiheftler <yossiheftler@gmail.com>
2021-11-21 21:09:49 +0200

    add

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
-def call(String name = 'yo') {
+def call(String name, String Date) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Hello, ${name}."
+    sh "echo Hello, ${name}. today is ${Date}"
 }
