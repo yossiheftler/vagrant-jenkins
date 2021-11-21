7c74f21f881f0ec2b84e5a399176662ad33d4fec
yossiheftler <yossiheftler@gmail.com>
2021-11-21 21:24:44 +0200

    change functin

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,9 @@
-def call(String name, String Date) {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    sh "echo Hello, ${name}. today is ${Date}"
+// def call(String name, String Date) {
+//     // Any valid steps can be called from this code, just like in other
+//     // Scripted Pipeline
+//     sh "echo Hello, ${name}. today is ${Date}"
+// }
+
+def call(Map config = [:]) {
+    sh "echo Hello ${config.name}, today is ${config.day}."
 }
