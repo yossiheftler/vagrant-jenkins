d7e0a380a4e05aa3b4b22db9fb27efcc7ad83187
yossiheftler <yossiheftler@gmail.com>
2022-07-21 14:31:32 +0300

    Change VM

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,3 +1,7 @@
+vagrant destroy -f
+vagrant up 
+ssh-keygen -f "/Users/yossi/.ssh/known_hosts" -R "192.168.56.20"
+
 sudo apt update
 sudo apt install -y openjdk-8-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
